bdc39a797dcfb93535a6556b782f45239d165b47
남의영 <ny2485@naver.com>
2025-02-19 20:44:03 +0900

    feat: add constuctors

--- src/main/java/young/refactoring/ch4/model/Producer.java
+++ src/main/java/young/refactoring/ch4/model/Producer.java
@@ -5,4 +5,18 @@ public class Producer {
     protected int cost;
     protected int production;
     protected Province province;
+
+    public Producer(String name, int cost, int production) {
+        this.name = name;
+        this.cost = cost;
+        this.production = production;
+    }
+
+    public Producer(String name, int cost, int production, Province province) {
+        this.name = name;
+        this.cost = cost;
+        this.production = production;
+        this.province = province;
+    }
+
 }
