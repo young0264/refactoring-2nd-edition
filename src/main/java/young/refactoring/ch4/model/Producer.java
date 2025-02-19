7d43de5acaf6913fdb27618eb1b2b1e52165b41c
남의영 <ny2485@naver.com>
2025-02-19 20:12:03 +0900

    feat: add basic models for Production Plan

---
+++ src/main/java/young/refactoring/ch4/model/Producer.java
@@ -0,0 +1,8 @@
+package young.refactoring.ch4.model;
+
+public class Producer {
+    protected String name;
+    protected int cost;
+    protected int production;
+    protected Province province;
+}
