6ee2e2ffbe6a2ef76ea84cbb796c8677e678d6cb
남의영 <ny2485@naver.com>
2025-02-20 13:07:24 +0900

    feat: add shortfall test

--- src/test/java/young/refactoring/ch4/ProductionPlanTest.java
+++ src/test/java/young/refactoring/ch4/ProductionPlanTest.java
@@ -1,16 +1,34 @@
 package young.refactoring.ch4;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import young.refactoring.ch4.model.Province;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductionPlanTest {
 
     @Test
+    @DisplayName("수요량 계산")
     void circDemandCostTest() {
         Province sampleProvinceData = ProductionSampleData.getSampleProvinceData();
         int demandCost = sampleProvinceData.getDemandCost();
-        Assertions.assertEquals(demandCost, 270);
+        assertEquals(demandCost, 270);
+    }
+
+    @Test
+    @DisplayName("생산 부족분 계산")
+    void shortfallTest() {
+        //given
+        Province sampleProvinceData = ProductionSampleData.getSampleProvinceData();
+        int answer = 5;
+
+        //when
+        int shortfall = sampleProvinceData.getShortfall();
+
+        //then
+        assertEquals(shortfall, answer);
     }
 
 }
