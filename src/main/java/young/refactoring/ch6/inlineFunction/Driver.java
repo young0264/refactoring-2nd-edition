13578d31eae233ca95868e48ab22d81aa19b44cd
남의영 <ny2485@naver.com>
2025-02-26 13:15:20 +0900

    feat: chapter6 Driver class 추가

---
+++ src/main/java/young/refactoring/ch6/inlineFunction/Driver.java
@@ -0,0 +1,9 @@
+package young.refactoring.ch6.inlineFunction;
+
+public class Driver {
+    public int numberOfLateDeliveries;
+
+    public Driver(int numberOfLateDeliveries) {
+        this.numberOfLateDeliveries = numberOfLateDeliveries;
+    }
+}
