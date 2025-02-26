3a9d871b8694530d60f70e92a75027a575223c70
남의영 <ny2485@naver.com>
2025-02-26 12:55:42 +0900

    refactor: 함수추출예시 완성

--- src/main/java/young/refactoring/ch6/extractFunction/ExtractFunctionExam.java
+++ src/main/java/young/refactoring/ch6/extractFunction/ExtractFunctionExam.java
@@ -1,20 +1,44 @@
 package young.refactoring.ch6.extractFunction;
 
+import java.time.*;
 
 public class ExtractFunctionExam {
 
-    public int printOwing(Invoice invoice) {
+    public void printOwing(Invoice invoice) {
 
+        printBanner();
+
+        int outstanding = calculateOutstanding(invoice);
+        recordDueDate(invoice);
+        printDetails(invoice, outstanding);
+    }
+
+    private static void printBanner() {
         System.out.println("*****************");
         System.out.println("**** 고객 채무 ****");
         System.out.println("*****************");
+    }
 
-        //미해결 채무(outstanding)를 계산
+    // 마감일 기록
+    private void recordDueDate(Invoice invoice) {
+        LocalDateTime now = LocalDateTime.now();
+        invoice.dueDate = now.plusDays(30);
+    }
+
+    //미해결 채무(outstanding)를 계산
+    private static int calculateOutstanding(Invoice invoice) {
         int outstanding = 0;
         for (Order o : invoice.getOrders()) {
             outstanding += o.getAmount();
         }
         return outstanding;
+    }
+
+    // 세부 사항을 출력
+    private void printDetails(Invoice invoice, int outstanding) {
+        System.out.println(String.format("고객명: %s", invoice.customer));
+        System.out.println(String.format("채무액: %d", outstanding));
+        System.out.println(String.format("마감일: %s", invoice.dueDate));
     }
 
 }
--- src/main/java/young/refactoring/ch6/extractFunction/Invoice.java
+++ src/main/java/young/refactoring/ch6/extractFunction/Invoice.java
@@ -1,13 +1,24 @@
 package young.refactoring.ch6.extractFunction;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Invoice {
 
     private List<Order> orders = new ArrayList<>();
+    protected LocalDateTime dueDate;
+    protected String customer;
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public String getCustomer() {
+        return customer;
     }
 }
