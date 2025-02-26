9d0038610a486a61d16d3330d9f08b25c015b444
남의영 <ny2485@naver.com>
2025-02-26 13:16:48 +0900

    refactor: 함수 인라인 리팩터링

--- src/main/java/young/refactoring/ch6/inlineFunction/InlineFunctionExam.java
+++ src/main/java/young/refactoring/ch6/inlineFunction/InlineFunctionExam.java
@@ -3,10 +3,7 @@ package young.refactoring.ch6.inlineFunction;
 public class InlineFunctionExam {
 
     public int rating(Driver driver){
-        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+        return driver.numberOfLateDeliveries > 5 ? 2 : 1; // 추출했던 함수를 원래 자리로.!
     }
 
-    public boolean moreThanFiveLateDeliveries(Driver driver) {
-        return driver.numberOfLateDeliveries > 5;
-    }
 }
