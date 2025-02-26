9af5923bc6bf9e09770470a9be33ddb7e2462392
남의영 <ny2485@naver.com>
2025-02-26 12:24:10 +0900

    refactor: 함수추출예시 클래스 내 선언문 이동(슬라이드)

--- src/main/java/young/refactoring/ch6/extractFunction/ExtractFunctionExam.java
+++ src/main/java/young/refactoring/ch6/extractFunction/ExtractFunctionExam.java
@@ -4,13 +4,13 @@ package young.refactoring.ch6.extractFunction;
 public class ExtractFunctionExam {
 
     public int printOwing(Invoice invoice) {
-        int outstanding = 0;
 
         System.out.println("*****************");
         System.out.println("**** 고객 채무 ****");
         System.out.println("*****************");
 
         //미해결 채무(outstanding)를 계산
+        int outstanding = 0;
         for (Order o : invoice.getOrders()) {
             outstanding += o.getAmount();
         }
