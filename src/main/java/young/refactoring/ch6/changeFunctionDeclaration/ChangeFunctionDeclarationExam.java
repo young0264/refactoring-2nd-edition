8ea5a6836fb74c06b47847111145398abb444732
남의영 <ny2485@naver.com>
2025-02-27 11:16:55 +0900

    feat: 마이그레이션 전 circum 메서드 추가

---
+++ src/main/java/young/refactoring/ch6/changeFunctionDeclaration/ChangeFunctionDeclarationExam.java
@@ -0,0 +1,9 @@
+package young.refactoring.ch6.changeFunctionDeclaration;
+
+public class ChangeFunctionDeclarationExam {
+
+    public double circum(double radius){
+        return 2 * Math.PI * radius;
+    }
+
+}
