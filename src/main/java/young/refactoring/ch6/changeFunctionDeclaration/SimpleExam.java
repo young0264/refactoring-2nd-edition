5069952aac704cdc20688313c1c23c0a497889c4
남의영 <ny2485@naver.com>
2025-02-27 10:52:07 +0900

    refactor: 함수이름 바꾸기 리팩터링

--- src/main/java/young/refactoring/ch6/changeFunctionDeclaration/SimpleExam.java
+++ src/main/java/young/refactoring/ch6/changeFunctionDeclaration/SimpleExam.java
@@ -2,7 +2,7 @@ package young.refactoring.ch6.changeFunctionDeclaration;
 
 public class SimpleExam {
 
-    public double circum(int radius){
+    public double circumference(int radius){
         return 2* Math.PI * radius;
     }
 }
