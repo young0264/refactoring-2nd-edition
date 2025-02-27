d354f209eccb6b41ea1abf4cf75c88a4f701e5f6
남의영 <ny2485@naver.com>
2025-02-27 15:58:47 +0900

    refactor: 매개변수 추가하기 예제 리팩터링

--- src/main/java/young/refactoring/ch6/changeFunctionDeclaration/AddParams.java
+++ src/main/java/young/refactoring/ch6/changeFunctionDeclaration/AddParams.java
@@ -7,6 +7,11 @@ public class AddParams {
     protected Queue<Customer> reservations = new ArrayDeque<>();
 
     public void addReservation(Customer customer) {
+        zz_addReservation(customer, false);
+    }
+
+    private void zz_addReservation(Customer customer, boolean isPriority) {
+        assert isPriority == true || isPriority == false;
         this.reservations.add(customer);
     }
 
