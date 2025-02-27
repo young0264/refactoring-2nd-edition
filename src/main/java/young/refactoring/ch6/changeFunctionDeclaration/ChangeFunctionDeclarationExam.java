9a6eb64e14f2415321eca75927a9013c86a09f9b
남의영 <ny2485@naver.com>
2025-02-27 12:42:03 +0900

    refactor: 변경 함수 선언 예제코드 초기화에 따른 수정

--- src/main/java/young/refactoring/ch6/changeFunctionDeclaration/ChangeFunctionDeclarationExam.java
+++ src/main/java/young/refactoring/ch6/changeFunctionDeclaration/ChangeFunctionDeclarationExam.java
@@ -2,12 +2,4 @@ package young.refactoring.ch6.changeFunctionDeclaration;
 
 public class ChangeFunctionDeclarationExam {
 
-    public double circum(double radius){
-        return circumference(radius);
-    }
-
-    public double circumference(double radius){
-        return 2* Math.PI * radius;
-    }
-
 }
--- src/test/java/young/refactoring/ch6/ChangeFunctionDeclarationExamTest.java
+++ src/test/java/young/refactoring/ch6/ChangeFunctionDeclarationExamTest.java
@@ -3,12 +3,13 @@ package young.refactoring.ch6;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import young.refactoring.ch6.changeFunctionDeclaration.ChangeFunctionDeclarationExam;
+import young.refactoring.ch6.changeFunctionDeclaration.MigrationExam;
 
 public class ChangeFunctionDeclarationExamTest {
 
     @Test
     void testCircum() {
-        ChangeFunctionDeclarationExam exam = new ChangeFunctionDeclarationExam();
+        MigrationExam exam = new MigrationExam();
         int radius = 5;
         double expected = 2 * Math.PI * radius;
         Assertions.assertEquals(expected, exam.circum(radius), 0.0001);
