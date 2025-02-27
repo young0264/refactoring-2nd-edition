23181b1b0b99bc4bab690d82aa0afb67a0a6b06c
남의영 <ny2485@naver.com>
2025-02-27 11:17:36 +0900

    refactor: 마이그레이션 후 circumference 함수 추출

--- src/main/java/young/refactoring/ch6/changeFunctionDeclaration/ChangeFunctionDeclarationExam.java
+++ src/main/java/young/refactoring/ch6/changeFunctionDeclaration/ChangeFunctionDeclarationExam.java
@@ -3,7 +3,11 @@ package young.refactoring.ch6.changeFunctionDeclaration;
 public class ChangeFunctionDeclarationExam {
 
     public double circum(double radius){
-        return 2 * Math.PI * radius;
+        return circumference(radius);
+    }
+
+    public double circumference(double radius){
+        return 2* Math.PI * radius;
     }
 
 }
