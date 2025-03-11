d301993673d00952e4467b6f407141f004a19e3b
남의영 <ny2485@naver.com>
2025-03-11 19:45:28 +0900

    comment: 초과 인출 이자로직 내 주석 추가

--- src/main/java/young/refactoring/ch8/moveFunction/AccountType.java
+++ src/main/java/young/refactoring/ch8/moveFunction/AccountType.java
@@ -5,13 +5,15 @@ public class AccountType {
     public boolean isPremium;
 
     public double overdraftCharge(int daysOverdrawn) {
+        // 프리미엄 멤버
         if (this.isPremium) {
             int baseCharge = 10;
             if (daysOverdrawn <= 7) {
                 return baseCharge;
-            } else {
+            } else { // 7일 이상이면 보다 낮은 이율 적용
                 return baseCharge + (daysOverdrawn - 7) * 0.85;
             }
+        // 일반 멤버
         } else {
             return daysOverdrawn * 1.75;
         }
