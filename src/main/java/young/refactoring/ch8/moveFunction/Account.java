f4019fa4606189353bdbcac989c16e6509559b3c
남의영 <ny2485@naver.com>
2025-03-11 19:27:36 +0900

    refactor: 8.1 Account 객체 record로 수정

--- src/main/java/young/refactoring/ch8/moveFunction/Account.java
+++ src/main/java/young/refactoring/ch8/moveFunction/Account.java
@@ -1,8 +1,14 @@
 package young.refactoring.ch8.moveFunction;
 
-public class Account {
-    private int dayOverdrawn; //(계좌별로 달라지는 변수), 특정날 초과인출?
-    private AccountType type;
+public record Account (
+        int dayOverdrawn, //(계좌별로 달라지는 변수), 특정날 초과인출?
+        AccountType type
+){
+
+    public Account(int dayOverdrawn, AccountType type) {
+        this.dayOverdrawn = dayOverdrawn;
+        this.type = type;
+    }
 
     /** 은행 이자 계산 */
     public double bankCharge(){
