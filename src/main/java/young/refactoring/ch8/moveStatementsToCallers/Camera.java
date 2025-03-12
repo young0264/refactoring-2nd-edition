3dbde4a809dc95a7d0465930a577542c8c5e5734
남의영 <ny2485@naver.com>
2025-03-12 09:13:45 +0900

    refactor: 8.4 문장을 호출한 곳으로 옮기기 - emitPhotoData -> zzNew 함수로 호출 이동

--- src/main/java/young/refactoring/ch8/moveStatementsToCallers/Camera.java
+++ src/main/java/young/refactoring/ch8/moveStatementsToCallers/Camera.java
@@ -10,7 +10,7 @@ public class Camera {
     public void renderPeron(OutputStream outputStream, Person person) throws IOException {
         outputStream.write(String.format("<p> %s </p>", person.name).getBytes());
         renderPhoto(outputStream, person.photo);
-        emitPhotoData(outputStream, person.photo);
+        zzNew(outputStream, person.photo);
         outputStream.write(String.format("<p> 날짜: %s </p>", person.photo.date.toString()).getBytes());
     }
 
@@ -20,7 +20,7 @@ public class Camera {
                 .forEach(p -> {
                     try {
                         outputStream.write("<div> \n".getBytes());
-                        emitPhotoData(outputStream, p);
+                        zzNew(outputStream, p);
                         outputStream.write(String.format("<p> 날짜: %s </p>", p.date.toString()).getBytes());
                         outputStream.write("</div>\n".getBytes());
                     } catch (IOException e) {
@@ -30,8 +30,8 @@ public class Camera {
     }
 
     private void emitPhotoData(OutputStream outputStream, Photo photo) throws IOException {
-        outputStream.write(String.format("<p> 제목: %s </p>", photo.title).getBytes());
-        outputStream.write(String.format("<p> 위치: %s </p>", photo.location).getBytes());
+        zzNew(outputStream, photo);
+        outputStream.write(String.format("<p> 날짜: %s </p>", photo.date.toString()).getBytes());
     }
 
     public String renderPhoto(OutputStream outputStream, Photo photo) {
@@ -40,6 +40,11 @@ public class Camera {
 
     private LocalDateTime recentDateCutOff() {
         return null;
+    }
+
+    private static void zzNew(OutputStream outputStream, Photo photo) throws IOException {
+        outputStream.write(String.format("<p> 제목: %s </p>", photo.title).getBytes());
+        outputStream.write(String.format("<p> 위치: %s </p>", photo.location).getBytes());
     }
 
 }
