cd9912da353bf1af9f8790168fd2c930c3e44944
남의영 <ny2485@naver.com>
2025-03-13 13:29:24 +0900

    refactor: 8.7 반복문 쪼개기 리팩터링

--- src/main/java/young/refactoring/ch8/splitLoop/SplitLoopExam.java
+++ src/main/java/young/refactoring/ch8/splitLoop/SplitLoopExam.java
@@ -8,8 +8,14 @@ public class SplitLoopExam {
         List<People> peopleList = getPeopleList();
         int youngest = peopleList.isEmpty() ? Integer.MAX_VALUE : peopleList.get(0).age;
         int totalSalary = 0;
+
+        // #1 반복문
         for (People p : peopleList) {
             if (p.age < youngest) youngest = p.age;
+        }
+
+        // #2 반복문
+        for (People p : peopleList) {
             totalSalary += p.salary;
         }
     }
