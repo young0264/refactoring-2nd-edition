93ff4fb6543536cdef265b113d714b1eeaf057af
남의영 <ny2485@naver.com>
2025-03-13 13:47:28 +0900

    feat: 8.8 반복문을 파이프라인으로 바꾸기 예제코드 추가

---
+++ src/main/java/young/refactoring/ch8/replaceLoopWithPipeline/Office.java
@@ -0,0 +1,11 @@
+package young.refactoring.ch8.replaceLoopWithPipeline;
+
+public class Office {
+    String city;
+    String phone;
+
+    public Office(String city, String phone) {
+        this.city = city;
+        this.phone = phone;
+    }
+}
