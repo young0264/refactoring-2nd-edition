4349201096481945cbca147059c3e2fe04559999
남의영 <ny2485@naver.com>
2025-03-17 18:40:12 +0900

    feat: add 'edit input parameter values' Exam

---
+++ src/main/java/young/refactoring/ch9/splitVariableExam/EditInputParameter.java
@@ -0,0 +1,11 @@
+package young.refactoring.ch9.splitVariableExam;
+
+public class EditInputParameter {
+
+    public int discount(int inputValue, int quantity) {
+        if (inputValue > 50) inputValue -= 2;
+        if (quantity > 100) inputValue -= 1;
+        return inputValue;
+    }
+
+}
