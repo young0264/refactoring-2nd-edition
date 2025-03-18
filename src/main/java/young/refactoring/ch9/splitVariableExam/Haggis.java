0ef664a33c1a8556ea9d1d55e2a4e679111ce499
남의영 <ny2485@naver.com>
2025-03-18 09:41:23 +0900

    feat: 챕터9 예제 Haggis 변수 쪼개기 step1

--- src/main/java/young/refactoring/ch9/splitVariableExam/Haggis.java
+++ src/main/java/young/refactoring/ch9/splitVariableExam/Haggis.java
@@ -6,16 +6,16 @@ public class Haggis {
     public double distanceTravelled(Scenario scenario, int time) {
         double result;
         // 초기힘 적용
-        int acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F)/질량(m)
+        int primaryAcceleration = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F)/질량(m)
         int primaryTime = Math.min(time, scenario.delay);
-        result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
 
         // 두번째 힘 적용
         double secondaryTime = time - scenario.delay;
         if (secondaryTime > 0) {
             // 두 번째 힘을 반영해 다시 계산
-            int primaryVelocity = acc * scenario.delay;
-            acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+            int primaryVelocity = primaryAcceleration * scenario.delay;
+            int acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
             result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
         }
         return result;
