fd4c0fe3aaa2a7cb5802ad3fa33d4e28a58c5df8
남의영 <ny2485@naver.com>
2025-03-19 09:41:15 +0900

    feat: 챕터 9-4 참조를 값으로 바꾸기 예제 코드 추가

---
+++ src/main/java/young/refactoring/ch9/changeReferenceTovalue/Person.java
@@ -0,0 +1,25 @@
+package young.refactoring.ch9.changeReferenceTovalue;
+
+public class Person {
+    TelephoneNumber telephoneNumber;
+
+    public Person(String areaCode, String number) {
+        telephoneNumber = new TelephoneNumber(areaCode, number);
+    }
+
+    public String getOfficeAreaCode() {
+        return telephoneNumber.areaCode;
+    }
+
+    public void setOfficeAreaCode(String areaCode) {
+        telephoneNumber = new TelephoneNumber(areaCode, telephoneNumber.number);
+    }
+
+    public String getOfficeNumber() {
+        return telephoneNumber.number;
+    }
+
+    public void setOfficeNumber(String number) {
+        telephoneNumber = new TelephoneNumber(telephoneNumber.areaCode, number);
+    }
+}
---
+++ src/main/java/young/refactoring/ch9/changeReferenceTovalue/TelephoneNumber.java
@@ -0,0 +1,28 @@
+package young.refactoring.ch9.changeReferenceTovalue;
+
+public class TelephoneNumber {
+
+    String areaCode;
+    String number;
+
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
