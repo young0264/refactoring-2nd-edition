8ae3987a280f21f93d6f6e13e0e64520a160c780
남의영 <ny2485@naver.com>
2025-03-20 19:46:06 +0900

    feat: 8.4 문장을 호출한 곳으로 옮기기 예제코드 수정

--- src/main/java/young/refactoring/ch8/moveStatementsToCallers/Person.java
+++ src/main/java/young/refactoring/ch8/moveStatementsToCallers/Person.java
@@ -3,4 +3,10 @@ package young.refactoring.ch8.moveStatementsToCallers;
 public class Person {
     String name;
     Photo photo;
+
+    public Person(String name, Photo photo) {
+        this.name = name;
+        this.photo = photo;
+    }
+
 }
