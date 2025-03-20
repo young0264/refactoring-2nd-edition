c6392f8109cce20538ec61393a639ddbc60c094b
남의영 <ny2485@naver.com>
2025-03-20 19:46:06 +0900

    feat: 8.4 문장을 호출한 곳으로 옮기기 예제코드 수정

--- src/main/java/young/refactoring/ch8/moveStatementsToCallers/Photo.java
+++ src/main/java/young/refactoring/ch8/moveStatementsToCallers/Photo.java
@@ -6,4 +6,11 @@ public class Photo {
     String title;
     String location;
     LocalDateTime date;
+
+    public Photo(String title, String location, LocalDateTime date) {
+        this.title = title;
+        this.location = location;
+        this.date = date;
+    }
+
 }
