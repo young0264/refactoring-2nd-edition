6f1f9ee4e15579b4a7cd94aab0b0b4a1803e2596
남의영 <ny2485@naver.com>
2025-03-24 13:33:30 +0900

    feat: Add Chapter 10, 조건문 분해 예시코드 추가

---
+++ src/main/java/young/refactoring/ch10/decomposeConditional/Plan.java
@@ -0,0 +1,11 @@
+package young.refactoring.ch10.decomposeConditional;
+
+import java.time.LocalDateTime;
+
+public class Plan {
+    LocalDateTime summerStart;
+    LocalDateTime summerEnd;
+    double summerRate;
+    double regularRate;
+    int regularServiceCharge;
+}
