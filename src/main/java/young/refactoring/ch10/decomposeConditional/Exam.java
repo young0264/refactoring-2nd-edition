31e8e7251076d425efd912e42fc63daa840b0673
남의영 <ny2485@naver.com>
2025-03-24 13:33:30 +0900

    feat: Add Chapter 10, 조건문 분해 예시코드 추가

---
+++ src/main/java/young/refactoring/ch10/decomposeConditional/Exam.java
@@ -0,0 +1,30 @@
+package young.refactoring.ch10.decomposeConditional;
+
+import java.time.LocalDateTime;
+
+public class Exam {
+    int quantity;
+    int charge;
+
+    public void calculatePayment(Plan plan, LocalDateTime dateTime) {
+        if (isSummer(dateTime, plan)) {
+            charge = summerCharge(plan);
+        } else {
+            charge = regularCharge(plan);
+        }
+    }
+
+    private boolean isSummer(LocalDateTime dateTime, Plan plan) {
+        return
+                !dateTime.isBefore(plan.summerStart) // 현재가 더 이전 날짜
+                && !dateTime.isAfter(plan.summerEnd); // 현재가 더 나중 날짜
+    }
+
+    private int summerCharge(Plan plan) {
+        return (int) (quantity * plan.summerRate);
+    }
+
+    private int regularCharge(Plan plan) {
+        return (int) (quantity * plan.regularRate + plan.regularServiceCharge);
+    }
+}
