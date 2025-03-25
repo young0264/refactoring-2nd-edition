c2f8621c1838bdbfcc2825e59931d419fb6a72e7
남의영 <ny2485@naver.com>
2025-03-25 12:01:13 +0900

    refactor: Chapter 10-2, 마지막 함수 추출 리팩터링 적용

--- src/main/java/young/refactoring/ch10/consolidateConditionalExpression/Exam.java
+++ src/main/java/young/refactoring/ch10/consolidateConditionalExpression/Exam.java
@@ -2,12 +2,17 @@ package young.refactoring.ch10.consolidateConditionalExpression;
 
 public class Exam {
     public int disabilityAmount(Employee employee) {
-        if(employee.seniority() < 2
-        || employee.monthDisabled() > 12
-        || employee.isPartTime())
+        if(isNotEligibleForDisability(employee))
             return 0;
 
         // 장애수단 계산
         return 0;
+    }
+
+    // 책에서는 중첩함수.
+    private static boolean isNotEligibleForDisability(Employee employee) {
+        return employee.seniority() < 2
+                || employee.monthDisabled() > 12
+                || employee.isPartTime();
     }
 }
