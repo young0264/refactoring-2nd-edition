232eb4776d784eade6eeba098d37db571fa32f2d
남의영 <ny2485@naver.com>
2025-03-25 11:58:21 +0900

    refactor: Chapter 10-2, 조건식 통합 리팩토링 적용 - 논리 연산자로 결합 - 조건식이 하나만 남을때 까지 결합

--- src/main/java/young/refactoring/ch10/consolidateConditionalExpression/Exam.java
+++ src/main/java/young/refactoring/ch10/consolidateConditionalExpression/Exam.java
@@ -2,9 +2,10 @@ package young.refactoring.ch10.consolidateConditionalExpression;
 
 public class Exam {
     public int disabilityAmount(Employee employee) {
-        if(employee.seniority() < 2) return 0;
-        if(employee.monthDisabled() > 12) return 0;
-        if(employee.isPartTime()) return 0;
+        if(employee.seniority() < 2
+        || employee.monthDisabled() > 12
+        || employee.isPartTime())
+            return 0;
 
         // 장애수단 계산
         return 0;
