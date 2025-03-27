ff1a087b3e3eafc1dfa57d2dacb5fafc713ddbad
남의영 <ny2485@naver.com>
2025-03-27 13:21:04 +0900

    feat: Chapter 7, 필드 초기값 추가 및 테스트 한글 설명 추가

--- src/main/java/young/refactoring/ch7/encapsulateRecord/CustomerDataStore.java
+++ src/main/java/young/refactoring/ch7/encapsulateRecord/CustomerDataStore.java
@@ -2,7 +2,7 @@ package young.refactoring.ch7.encapsulateRecord;
 
 public class CustomerDataStore {
 
-    private static CustomerData customerData = new CustomerData();
+    private static CustomerData customerData;
 
     public static CustomerData getCustomerData() {
         return customerData;
