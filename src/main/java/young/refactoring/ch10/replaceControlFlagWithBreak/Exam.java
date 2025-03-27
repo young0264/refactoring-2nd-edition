8ddd497963ce17e61c5cd40111dc19fa8441d5c9
남의영 <ny2485@naver.com>
2025-03-27 11:10:27 +0900

    refactor: Chapter 10-7, 함수제어 플래그 제거 리팩터링.

--- src/main/java/young/refactoring/ch10/replaceControlFlagWithBreak/Exam.java
+++ src/main/java/young/refactoring/ch10/replaceControlFlagWithBreak/Exam.java
@@ -3,16 +3,11 @@ package young.refactoring.ch10.replaceControlFlagWithBreak;
 public class Exam {
 
     public String checkForMiscreants(People people) {
-        boolean found = false;
         for (Person person : people.people()) {
-            if (!found) {
-                if (person.name().equals("조커")) {
-                    found = true;
-                    return sendAert();
-                }else if (person.name().equals("사루만")) {
-                    found = true;
-                    return sendAert();
-                }
+            if (person.name().equals("조커")) {
+                return sendAert();
+            }else if (person.name().equals("사루만")) {
+                return sendAert();
             }
         }
         return "신원불명";
