e76720cca1c85fc1e0ee03b7dfadafdb4f519ab7
남의영 <ny2485@naver.com>
2025-03-31 09:58:56 +0900

    refactor: 10.6 introduce Assertion exam

--- src/main/java/young/refactoring/ch10/introduceAssertion/Customer.java
+++ src/main/java/young/refactoring/ch10/introduceAssertion/Customer.java
@@ -5,10 +5,15 @@ public class Customer {
 
     double discountRate;
 
-    // 할인율이 항상 양수라는 가정.
-    // 하지만 음수가 가능한 메서드(책 기준 코드)
+    // [setter] discountRate가 처음 할당되는 곳
+    public void setDiscountRate(double discountRate) {
+        Assert.isTrue(discountRate > 0 && discountRate <= 1
+                , "할인율은 0보다 크고 1보다 작아야 합니다.");
+        this.discountRate = discountRate;
+    }
+
+    // 할인율이 항상 양수라는 가정 _ 할인률이 100프로가 넘으면 음수가 가능한 메서드(책 기준 코드)
     public double applyDiscountAllowingOver100Percent(int aNumber) {
-        // 1.0, 100% 미만이어야함.
         if (this.discountRate > 0)
             return aNumber - this.discountRate * aNumber;
         return aNumber;
@@ -27,12 +32,6 @@ public class Customer {
         if (this.discountRate > 0 && this.discountRate <= 1)
             return aNumber - this.discountRate * aNumber;
         return aNumber;
-    }
-
-    // setter, discountRate가 처음 할당되는 곳
-    public void setDiscountRate(double discountRate) {
-        Assert.isTrue(discountRate > 0, "할인율은 0보다 커야합니다.");
-        this.discountRate = discountRate;
     }
 
 }
--- src/test/java/young/refactoring/ch10/introduceAssertion/CustomerTest.java
+++ src/test/java/young/refactoring/ch10/introduceAssertion/CustomerTest.java
@@ -1,15 +1,22 @@
 package young.refactoring.ch10.introduceAssertion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
 
+    Customer customer ;
+
+    @BeforeEach
+    void setUp() {
+        customer = new Customer();
+    }
+
     @Test
     @DisplayName("[over 100] 할인율이 양수일 경우 할인 금액이 적용된다")
     void applyDiscount_AllowingOver100Percent_whenRateIsValid_returnsDiscountedAmount() {
-        Customer customer = new Customer();
         customer.setDiscountRate(0.2); // 20% 할인
 
         double result = customer.applyDiscountAllowingOver100Percent(100); // 100 - 20 = 80
@@ -19,8 +26,8 @@ class CustomerTest {
     @Test
     @DisplayName("[over 100] 할인율이 0일 경우 원래 금액이 반환된다")
     void applyDiscount_AllowingOver100Percent_whenRateIsZero_returnsOriginalAmount() {
-        Customer customer = new Customer();
-        customer.setDiscountRate(0.0); // 할인 없음
+        // 할인 없음
+        assertThrows(IllegalArgumentException.class, () -> customer.setDiscountRate(0.0));
 
         double result = customer.applyDiscountAllowingOver100Percent(100);
         assertEquals(100.0, result, 0.0001);
@@ -29,8 +36,8 @@ class CustomerTest {
     @Test
     @DisplayName("[over 100] 할인율이 음수일 경우 할인되지 않고 원래 금액이 반환된다")
     void applyDiscount_AllowingOver100Percent_whenRateIsNegative_returnsOriginalAmount() {
-        Customer customer = new Customer();
-        customer.setDiscountRate(-0.1); // 잘못된 값 -> 로직상 그대로 반환
+        // 잘못된 값 -> 로직상 그대로 반환
+        assertThrows(IllegalArgumentException.class, () -> customer.setDiscountRate(-0.1));
 
         double result = customer.applyDiscountAllowingOver100Percent(100);
         assertEquals(100.0, result, 0.0001);
@@ -39,8 +46,8 @@ class CustomerTest {
     @Test
     @DisplayName("[under 100] 할인율이 1.0 초과일 경우 금액이 음수가 될 수 없다")
     void applyDiscount_AllowingOver100Percent_whenRateIsAboveOne_returnsNegativeAmount() {
-        Customer customer = new Customer();
-        customer.setDiscountRate(1.2); // 120% 할인 -> 비정상 할인 -> 기본 값 return
+        // 120% 할인 -> 비정상 할인 -> 기본 값 return
+        assertThrows(IllegalArgumentException.class, () -> customer.setDiscountRate(1.2));
 
         double result = customer.applyDiscountUnder100Percent(100);
         assertEquals(100, result, 0.0001);
@@ -49,7 +56,6 @@ class CustomerTest {
     @Test
     @DisplayName("[under 100] 할인율이 1.0일 경우 금액이 0이 되어야 한다")
     void applyDiscount_AllowingUnder100Percent_whenRateIsOne_returnsZero() {
-        Customer customer = new Customer();
         customer.setDiscountRate(1.0); // 100% 할인
 
         double result = customer.applyDiscountUnder100Percent(100);
@@ -59,9 +65,8 @@ class CustomerTest {
     @Test
     @DisplayName("[under 100] 할인율이 아주 작을 경우 거의 원래 금액과 동일해야 한다")
     void applyDiscount_AllowingUnder100Percent_whenRateIsTiny_returnsAlmostOriginal() {
-        Customer customer = new Customer();
-        customer.setDiscountRate(0); // 거의 0
-
+        // 거의 0
+        assertThrows(IllegalArgumentException.class, () -> customer.setDiscountRate(0));
         double result = customer.applyDiscountUnder100Percent(100);
         assertEquals(100.0,result, 0.0001); // 매우 작게 깎임
     }
@@ -72,20 +77,13 @@ class CustomerTest {
         Customer customerA = new Customer();
         Customer customerB = new Customer();
 
-        customerA.setDiscountRate(Double.NaN); // Nan 일 때
-        customerB.setDiscountRate(Double.POSITIVE_INFINITY); // Infinity 일 때
-
-        double resultA = customerA.applyDiscountUnder100Percent(100);
-        double resultB = customerB.applyDiscountUnder100Percent(100);
-
-        assertEquals(100.0, resultA, 0.0001);
-        assertEquals(100.0, resultB, 0.0001);
+        assertThrows(IllegalArgumentException.class, () -> customerA.setDiscountRate(Double.NaN));
+        assertThrows(IllegalArgumentException.class, () -> customerB.setDiscountRate(Double.POSITIVE_INFINITY));
     }
 
     @Test
     @DisplayName("[under 100] 할인 대상 금액이 0일 경우 항상 0을 반환한다")
     void applyDiscount_whenBaseAmountIsZero_returnsZero() {
-        Customer customer = new Customer();
         customer.setDiscountRate(0.5);
 
         double result = customer.applyDiscountUnder100Percent(0);
@@ -95,7 +93,6 @@ class CustomerTest {
     @Test
     @DisplayName("[under 100] 할인 대상 금액이 음수일 경우 그대로 할인 계산된다")
     void applyDiscount_whenBaseAmountIsNegative_returnsDiscountedNegative() {
-        Customer customer = new Customer();
         customer.setDiscountRate(0.2);
 
         double result = customer.applyDiscountUnder100Percent(-100);
@@ -105,11 +102,11 @@ class CustomerTest {
     @Test
     @DisplayName("[price non minus] 금액이 음수일 경우 예외 발생")
     void applyDiscount_negativeAmount_throwsException() {
-        Customer customer = new Customer();
         customer.setDiscountRate(0.5);
 
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-            customer.applyDiscountWithNonNegativeInputs(-100);
+        IllegalArgumentException illegalArgumentException =
+                assertThrows(IllegalArgumentException.class, () -> {
+                    customer.applyDiscountWithNonNegativeInputs(-100);
         });
         assertEquals("할인 대상 금액은 음수가 될 수 없습니다.", illegalArgumentException.getMessage());
     }
