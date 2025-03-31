439a7f0a264eeef116a4732052178389bae3d476
남의영 <ny2485@naver.com>
2025-03-31 10:00:10 +0900

    feat: 11.1 질의 함수와 변경 함수 분리하기 exam

---
+++ src/main/java/young/refactoring/ch11/separteQueryFromModifier/Exam.java
@@ -0,0 +1,36 @@
+package young.refactoring.ch11.separteQueryFromModifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam {
+    List<Person> people = new ArrayList<>();
+
+    public String alertForMiscreant() {
+        for (Person p : people) {
+            if (p.name().equals("조커")) {
+                return "조커";
+            }
+            if (p.name().equals("사루만")) {
+                return "사루만";
+            }
+        }
+        return "";
+    }
+
+    public String findForMiscreant(){
+        for (Person p : people) {
+            if (p.name().equals("조커")) {
+                return "조커";
+            }
+            if (p.name().equals("사루만")) {
+                return "사루만";
+            }
+        }
+        return "";
+    }
+
+    private void setOffAlarms() {
+
+    }
+}
