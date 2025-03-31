3fe037962e4116f359eab2e90bf1bb861ed08113
남의영 <ny2485@naver.com>
2025-03-31 10:14:41 +0900

    feat: 11.1 테스트 추가

---
+++ src/test/java/young/refactoring/ch11/separteQueryFromModifier/ExamTest.java
@@ -0,0 +1,55 @@
+package young.refactoring.ch11.separteQueryFromModifier;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamTest {
+    private Exam exam;
+
+    @BeforeEach
+    void setUp() {
+        exam = new Exam();
+    }
+
+    @Test
+    @DisplayName("범죄자가 없으면 빈 문자열을 반환한다")
+    void returnsEmptyWhenNoMiscreantFound() {
+        exam.people = List.of(
+                new Person("Batman"),
+                new Person("Gandalf")
+        );
+
+        String result = exam.findForMiscreant();
+
+        assertEquals("", result);
+    }
+
+    @Test
+    @DisplayName("조커가 먼저 있으면 '조커'를 반환한다")
+    void returnsJokerWhenJokerIsPresent() {
+        exam.people = List.of(
+                new Person("조커"),
+                new Person("사루만")
+        );
+
+        String result = exam.findForMiscreant();
+
+        assertEquals("조커", result);
+    }
+
+    @Test
+    @DisplayName("사루만만 있으면 '사루만'을 반환한다")
+    void returnsSarumanWhenOnlySarumanIsPresent() {
+        exam.people = List.of(new Person("사루만"));
+
+        String result = exam.findForMiscreant();
+
+        assertEquals("사루만", result);
+    }
+
+}
