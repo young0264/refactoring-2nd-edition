f63968e904019c91cf47c5cef288ac08047131cd
남의영 <ny2485@naver.com>
2025-04-03 20:35:01 +0900

    feat: 11-2 함수 매개변수화하기 어려운 기본 예제 추가

---
+++ src/main/java/young/refactoring/ch11/parameterizeFunction/Exam2.java
@@ -0,0 +1,31 @@
+package young.refactoring.ch11.parameterizeFunction;
+
+public class Exam2 {
+
+    public int baseCharge(int usage) {
+        if (usage < 0) return 0;
+
+        int amount =
+                (int) (bottomBand(usage) * 0.03
+                        + middleBand(usage) * 0.05
+                        + topBand(usage) * 0.07);
+
+        return usd(amount);
+    }
+
+    private int usd(int amount) {
+        return amount;
+    }
+
+    private double topBand(int usage) {
+        return usage > 200 ? usage - 200 : 0;
+    }
+
+    private double middleBand(int usage) {
+        return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+    }
+
+    private int bottomBand(int usage) {
+        return Math.min(usage, 0);
+    }
+}
