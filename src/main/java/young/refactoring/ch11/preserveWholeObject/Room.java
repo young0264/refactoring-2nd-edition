2ab58c47049295dcc12eba7d11e39dfd80de3f41
남의영 <ny2485@naver.com>
2025-04-03 10:15:55 +0900

    refactor: 11-4. 객체 통째로 넘기기 리팩터링 및 테스트 추가

--- src/main/java/young/refactoring/ch11/preserveWholeObject/Room.java
+++ src/main/java/young/refactoring/ch11/preserveWholeObject/Room.java
@@ -3,4 +3,8 @@ package young.refactoring.ch11.preserveWholeObject;
 public class Room {
 
     public Range daysTempRange;
+
+    public Room(Range daysTempRange) {
+        this.daysTempRange = daysTempRange;
+    }
 }
