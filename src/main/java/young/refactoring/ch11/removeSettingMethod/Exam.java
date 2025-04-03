7c353c8181ae5166c1e26b41499c7a449f82cd66
남의영 <ny2485@naver.com>
2025-04-03 12:40:45 +0900

    refactor: 11-7. 세터 제거하기 - 생성자를 통한 값 setting

--- src/main/java/young/refactoring/ch11/removeSettingMethod/Exam.java
+++ src/main/java/young/refactoring/ch11/removeSettingMethod/Exam.java
@@ -3,10 +3,7 @@ package young.refactoring.ch11.removeSettingMethod;
 public class Exam {
 
     public void client() {
-        Person person = new Person();
-
-        person.id = 1234;
-        person.name = "마틴";
+        Person person = new Person(1234, "마틴");
     }
 
 }
