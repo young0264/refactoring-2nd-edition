638823b504bd3732e8efc6e0c0a182360b48e890
남의영 <ny2485@naver.com>
2025-04-06 18:35:17 +0900

    refactor: 11.10 명령을 함수로 바꾸기

--- src/main/java/young/refactoring/ch11/replaceCommandWithFunction/Exam.java
+++ src/main/java/young/refactoring/ch11/replaceCommandWithFunction/Exam.java
@@ -1,12 +1,18 @@
 package young.refactoring.ch11.replaceCommandWithFunction;
 
+/**
+ * [명령을 함수로 바꾸기]
+ * - 객체화할 이유가 없는 경우
+ *   -> 클래스 분리가 오히려 가독성과 유지보수를 어렵게 함
+ **/
 public class Exam {
 
-    public void client(Customer customer, int usage, Provider provider) {
-        charge(customer, usage, provider);
+    public double client(Customer customer, int usage, Provider provider) {
+        double monthCharge = charge(customer, usage, provider);
+        return monthCharge;
     }
 
-    private void charge(Customer customer, int usage, Provider provider) {
-        double monthCharge = new ChargeCalculator(customer, usage, provider).charge();
+    private double charge(Customer customer, int usage, Provider provider) {
+        return new ChargeCalculator(customer, usage, provider).charge();
     }
 }
